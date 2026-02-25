8d929c6e8b3b3bba08249b640e2e22f99e41123b
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2026-02-25 10:07:33 -0600

    Update gravity.hpp

--- gravity.hpp
+++ gravity.hpp
@@ -3,7 +3,7 @@
 
 #include "universe.hpp"
 
-// gravitational constant
-const float GRAV_CONST;
+// gravitational constant, based on newest scientific gravitational constant
+const float GRAV_CONST = 6.674.0f;
 
 #endif
